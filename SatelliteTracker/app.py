14ea37b5e32bba1d7c4481cbc5239cc2a1104b16
PeledGuy <guy.peled1234@gmail.com>
2026-06-28 20:34:50 +0300

    Added ability to choose location.

--- SatelliteTracker/app.py
+++ SatelliteTracker/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, jsonify, render_template, request
 from orbit_engine import OrbitEngine
+import urllib.request
+
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')]
+urllib.request.install_opener(opener)
 
 # Initialize the Web App and Physics Engine
 app = Flask(__name__)
